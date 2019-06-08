8c20f6df87b00f2330d5e106d010c2c0d2797faa
Lachezar Kutsarov <lkutsarov@gmail.com>
2019-06-08 21:28:41 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -16,9 +16,9 @@ public class Solution {
 			int numberOfChipsAtPile = 0;
 			for (int j = 0; j < numberOfPiles; j++) {
 				/**
-				 * Since each player has an equal number of times to add chips to a pile, an
-				 * addition of chips from one player can be negated with an addition from 
-				 * the other player.
+				 * The addition of chips from one player can be negated with a removal or an
+				 * addition from the other. Therefore, the value of "maximumAdditionsPerPlayer"
+				 * has no influence on the results.
 				 * 
 				 * Thus, XOR operations for the standard Nim Game (at each turn, removing at
 				 * least one element from a pile, up to all contained in the pile) can be
